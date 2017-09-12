f0b56070c3be011dbf4182b625f3163e286baacc
Maria Mata <mariajo.mata@gmail.com>
2017-09-12 14:53:48 -0600

    GET all & GET by ID ✔️

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # ----- IMPORTS -----
-import sqlite3
+import sqlite3, json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -9,7 +9,8 @@ app = Flask(__name__)
 @app.route('/api/songs', methods=['GET', 'POST'])
 def collection():
     if request.method == 'GET':
-        pass # handle GET all request
+        all_songs = get_all_songs()
+        return json.dumps(all_songs)
     elif request.method == 'POST':
         data = request.form
         result = add_song(data['artist'], data['title'], data['rating'])
@@ -19,7 +20,8 @@ def collection():
 @app.route('/api/songs/<song_id>', methods=['GET', 'PUT', 'DELETE'])
 def resource(song_id):
     if request.method == 'GET':
-        pass # handle GET song by ID
+        song = get_single_song(song_id)
+        return json.dumps(song)
     elif request.method == 'PUT':
         pass # handle PUT song by ID
     elif request.method == 'DELETE':
@@ -36,6 +38,20 @@ def add_song(artist, title, rating):
     except:
         result = {'status': 0, 'message': 'Error'}
     return result
+
+def get_all_songs():
+    with sqlite3.connect('songs.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM songs ORDER BY id desc")
+        all_songs = cursor.fetchall()
+        return all_songs
+
+def get_single_song(song_id):
+    with sqlite3.connect('songs.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM songs WHERE id = ?", (song_id,))
+        song = cursor.fetchone()
+        return song
 
 # Below states that this source file is our main program
 # Any files imported from other modules will have their name set to their module name
